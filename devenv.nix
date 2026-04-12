ce3c379ff5c34cb5069595e76c0347d80179b634
Danju Visvanathan <danju.visvanathan@gmail.com>
2026-04-12 19:41:55 +1000

    fix: add packages to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,12 +1,17 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  packages = [ 
-    pkgs.git 
+  packages = [
+    pkgs.git
     pkgs.kubectl
     pkgs.helm
+    pkgs.kustomize
+    pkgs.kubeseal
+    pkgs.argocd
     pkgs.k9s
+    pkgs.bazelisk
     pkgs.claude-code
     pkgs.python3
+    pkgs.uv
   ];
 }
